4131199c2ff76ac911fb9b0160e841750b6b87a0
jonatanspo <127948186+jonatanspo@users.noreply.github.com>
2023-09-22 20:41:54 +0000

    changes

--- main.py
+++ main.py
@@ -16,7 +16,47 @@ with open('min_max_scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
 # Funktion zum Scrapen der OHLC-Daten von der Börsen-Website
-# (Code für das Scraping bleibt unverändert)
+def scrape_ohlc_data():
+    url = 'https://finance.yahoo.com/quote/AAPL/history?p=AAPL'
+
+    try:
+        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        headers = {'User-Agent': user_agent}
+
+        retries = 3  # Anzahl der Wiederholungsversuche
+        for _ in range(retries):
+            response = requests.get(url, headers=headers, timeout=10)
+            if response.status_code == 200:
+                break
+            else:
+                time.sleep(5)  # Warten Sie vor dem erneuten Versuch
+        else:
+            raise RequestException("Maximale Anzahl von Wiederholungsversuchen erreicht.")
+
+        response.raise_for_status()
+
+        page_content = response.content
+        soup = BeautifulSoup(page_content, 'html.parser')
+        table = soup.find_all('table')[0]
+        rows = table.find_all('tr')
+
+        # Überprüfen, ob die Tabelle Daten enthält
+        if len(rows) < 2:
+            raise RequestException("Die Tabelle enthält keine Daten.")
+
+        # Hier erhalten wir den letzten Eintrag in der Tabelle
+        ohlc_row = rows[1]  # Ändern Sie die Zeilennummer auf 1, um die zweite Zeile zu erhalten
+        ohlc_data = ohlc_row.find_all('td')
+        open_price = float(ohlc_data[1].text)
+        high_price = float(ohlc_data[2].text)
+        low_price = float(ohlc_data[3].text)
+        close_price = float(ohlc_data[4].text)
+        volume = float(ohlc_data[6].text.strip().replace(',', ''))  # Handelsvolumen aus der siebten Spalte (Index 6)
+
+        return open_price, high_price, low_price, close_price, volume
+    except RequestException as e:
+        print(f"Fehler bei der HTTP-Anfrage: {e}")
+        return None
 
 # Streamlit-Anwendung
 st.title('Apple Inc. Aktienkursprognose')
@@ -45,7 +85,8 @@ else:
 # Eingabefelder für die Prognose
 st.write("### Eingabefelder für die Prognose")
 
-# (Code für die Eingabefelder bleibt unverändert)
+p6 = st.number_input("Prozentuale Veränderung des NASDAQ Composite Index", step=0.01)
+p7 = st.number_input("20 Tage EMA der Apple Aktie", min_value=0.00, step=0.01)
 
 if st.button("Vorhersage"):
     # Füge die Benutzereingaben zu DataFrame hinzu
