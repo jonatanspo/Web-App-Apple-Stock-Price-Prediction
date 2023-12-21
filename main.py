45ff24aa19782fb1756c6ece4f179a8484d261a8
Jonatan Spohn <127948186+jonatanspo@users.noreply.github.com>
2023-12-21 16:09:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -42,35 +42,35 @@ def scrape_ohlc_data():
         if len(rows) < 2:
             raise RequestException("The table contains no data.")
 
- 
-        ohlc_row = rows[1] 
+        ohlc_row = rows[1]
         ohlc_data = ohlc_row.find_all('td')
-        open_price = float(ohlc_data[1].text)
-        high_price = float(ohlc_data[2].text)
-        low_price = float(ohlc_data[3].text)
-        close_price = float(ohlc_data[4].text)
+        open_price = float(ohlc_data[1].text.replace(',', ''))
+        high_price = float(ohlc_data[2].text.replace(',', ''))
+        low_price = float(ohlc_data[3].text.replace(',', ''))
+        close_price = float(ohlc_data[4].text.replace(',', ''))
         volume = float(ohlc_data[6].text.strip().replace(',', ''))
 
         return open_price, high_price, low_price, close_price, volume
     except RequestException as e:
-        print(f"Fehler bei der HTTP-Anfrage: {e}")
+        print(f"HTTP request error: {e}")
         return None
-    def scrape_nasdaq():
+
+def scrape_nasdaq():
     url = 'https://finance.yahoo.com/quote/%5EIXIC/history?p=%5EIXIC'
 
     try:
         user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
         headers = {'User-Agent': user_agent}
 
-        retries = 3  # Anzahl der Wiederholungsversuche
+        retries = 3  # Number of retry attempts
         for _ in range(retries):
             response = requests.get(url, headers=headers, timeout=10)
             if response.status_code == 200:
                 break
             else:
-                time.sleep(5)  # Warten Sie vor dem erneuten Versuch
+                time.sleep(5)  # Wait before retrying
         else:
-            raise RequestException("Maximale Anzahl von Wiederholungsversuchen erreicht.")
+            raise RequestException("Maximum number of retry attempts reached.")
 
         response.raise_for_status()
 
@@ -79,11 +79,11 @@ def scrape_ohlc_data():
         table_nasdaq = soup.find_all('table')[0]
         rows_nasdaq = table_nasdaq.find_all('tr')
 
-        # Überprüfen, ob die Tabelle Daten enthält
-        if len(rows_nasdaq) < 3:  # Die Tabelle sollte mindestens 3 Zeilen haben (Header + 2 Dateneinträge)
-            raise RequestException("Die Tabelle enthält keine ausreichenden Daten.")
+        # Check if the table contains data
+        if len(rows_nasdaq) < 3:  # The table should have at least 3 rows (Header + 2 data entries)
+            raise RequestException("The table does not contain sufficient data.")
 
-        # Hier erhalten wir den letzten Eintrag in der Tabelle
+        # Get the last entry in the table
         nasdaq_row_latest = rows_nasdaq[1]
         nasdaq_row_previous = rows_nasdaq[2]
         nasdaq_data_latest = nasdaq_row_latest.find_all('td')
@@ -92,23 +92,22 @@ def scrape_ohlc_data():
         latest_close_price = float(latest_close_price_cleaned_string)
         previous_close_price_cleaned_string = nasdaq_data_previous[4].text.replace(',', '')
         previous_close_price = float(previous_close_price_cleaned_string)
-        
-        # Berechnung der Änderung in IXIC
-        result = ((latest_close_price - previous_close_price) / previous_close_price)*100
+
+        # Calculate the change in IXIC
+        result = ((latest_close_price - previous_close_price) / previous_close_price) * 100
 
         return result
     except RequestException as e:
-        print(f"Fehler bei der HTTP-Anfrage: {e}")
+        print(f"HTTP request error: {e}")
         return None
 
-# Testen Sie die Funktion
+# Test the function
 change_percentage = scrape_nasdaq()
 if change_percentage is not None:
-    print(f"Änderungsprozentsatz in IXIC: {change_percentage:.2f}%")
+    print(f"NASDAQ change percentage: {change_percentage:.2f}%")
 else:
-    print("Fehler beim Abrufen der Daten.")
+    print("Failed to retrieve the data.")
 
-    
 def scrape_ema_20(): 
     url = 'https://financhill.com/stock-price-chart/aapl-technical-analysis'
 
@@ -116,15 +115,15 @@ def scrape_ema_20():
         user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
         headers = {'User-Agent': user_agent}
 
-        retries = 3  # Anzahl der Wiederholungsversuche
+        retries = 3  # Number of retry attempts
         for _ in range(retries):
             response = requests.get(url, headers=headers, timeout=10)
             if response.status_code == 200:
                 break
             else:
-                time.sleep(5)  # Warten Sie vor dem erneuten Versuch
+                time.sleep(5)  # Wait before retrying
         else:
-            raise RequestException("Maximale Anzahl von Wiederholungsversuchen erreicht.")
+            raise RequestException("Maximum number of retry attempts reached.")
 
         response.raise_for_status()
 
@@ -133,38 +132,38 @@ def scrape_ema_20():
         table_ema_20 = soup.find('table',{"class":"stock-info-table"})
         rows_ema = table_ema_20.find_all('tr')
 
-        # Hier erhalten wir den letzten Eintrag in der Tabelle
+        # Get the last entry in the table
         ema_20_row = rows_ema[2]
         ema_20_data = ema_20_row.find_all('td')
-        ema_20 = float(ema_20_data[0].text)
+        ema_20 = float(ema_20_data[1].text)
 
         return ema_20
     except RequestException as e:
-        print(f"Fehler bei der HTTP-Anfrage: {e}")
+        print(f"HTTP request error: {e}")
         return None
-     
-# Streamlit-Anwendung
-st.title('Apple Inc. Aktienkursprognose')
 
-# Automatisches Scraping beim Laden der App
+# Streamlit application
+st.title('Apple Inc. Stock Price Forecast')
+
+# Automatic scraping when the app loads
 ohlc_data_new = scrape_ohlc_data()
 nasdaq = scrape_nasdaq()
 ema = scrape_ema_20()
 
 if ohlc_data_new:
-    st.write("Gescrapte OHLC-Daten:")
+    st.write("Scraped OHLC data:")
     
-    # Ändern Sie nasdaq und ema in Listen mit einem einzigen Element
+    # Change nasdaq and ema into lists with a single element
     nasdaq_list = [nasdaq]
     ema_list = [ema]
     
     ohlc_table = pd.DataFrame({
-        "Kennzahl": ["Open", "High", "Low", "Close", "Volume", "IXIC", "ema_20"],
-        "Wert": [ohlc_data_new[0], ohlc_data_new[1], ohlc_data_new[2], ohlc_data_new[3], ohlc_data_new[4], nasdaq_list[0], ema_list[0]]
-    }).set_index("Kennzahl")  # Hier setzen wir die "Kennzahl" Spalte als Index
+        "Metric": ["Open", "High", "Low", "Close", "Volume", "IXIC", "ema_20"],
+        "Value": [ohlc_data_new[0], ohlc_data_new[1], ohlc_data_new[2], ohlc_data_new[3], ohlc_data_new[4], nasdaq_list[0], ema_list[0]]
+    }).set_index("Metric")  # Here we set the "Metric" column as the index
     st.table(ohlc_table)
 
-    # Erstellen eines DataFrame aus den gescrapten Daten
+    # Create a DataFrame from the scraped data
     df = pd.DataFrame({
         "Open": [ohlc_data_new[0]],
         "High": [ohlc_data_new[1]],
@@ -175,17 +174,16 @@ if ohlc_data_new:
         "ema_20": [ema_list[0]],
     })
 
-
-    if st.button("Vorhersage"):
+    if st.button("Predict"):
         
-        # Anwendung des MinMaxScaler auf die eingegebenen Daten
+        # Apply MinMaxScaler to the input data
         input_data_scaled = scaler.transform(df)
 
-        # Vorhersage mit dem Modell
-        pred = svr_model.predict(input_data_scaled)
-        st.success("Der Schlusskurs der Apple Aktie wird morgen Abend {:.2f}$ betragen".format(pred[0]))
+        # Prediction with the model
+        prediction = svr_model.predict(input_data_scaled)
+        st.success(f"The closing price of Apple stock is predicted to be ${prediction[0]:.2f} by the end of tomorrow.")
 
-        if pred > df["Close"].values[0]:
-            st.success("Laut dieser Prognose ist es sinnvoll in die Apple Aktie zu investieren, da der morgige Schlusskurs vermutlich höher ist als der heutige!")
+        if prediction > df["Close"].values[0]:
+            st.success("According to this forecast, it might be a good idea to invest in Apple stock, as tomorrow's closing price is expected to be higher than today's!")
         else:
-            st.warning("Der Schlusskurs von morgen Abend ist niedriger oder gleich hoch wie der Schlusskurs")
+            st.warning("Tomorrow's evening closing price is predicted to be lower or equal to today's closing price.")
